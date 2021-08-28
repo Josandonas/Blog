866d7a34de05a22d4b3318b8cd138cadb91f8018
Josandonas <josandonas@outlook.com>
2021-08-28 01:06:51 -0400

    alterando caminho da imagem, alterando admin no seed e definindo estatus de nivel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->string('name');
-            $table->string('email')->nunable();
-            $table->timestamp('email_verified_at')->nunable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,10 +6,10 @@ class UsersTableSeeder extends Seeder{
 
     public function run(){
     	 DB::table('users')->insert([
-    	 	'id'=>'1',
             'name' => 'José Sandonas',
             'email' => 'josandonas98@gmail.com',
             'password' => bcrypt('hahahaha'),
+            'role'=> 'Admin'
         ]);
     }
 }
