b29aea9ccb7e4d5ca74f7fdbf8a1700f128c165b
Josandonas <josandonas@outlook.com>
2021-09-09 22:38:48 -0400

    alterações na página de sobre antes de login

--- resources/views/about2.blade.php
+++ resources/views/about2.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 @section('content')
 
-<header class="masthead-4">
+<div class="masthead-4">
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="site-heading">
-          <h1>Sobre o Site</h1>
-          <span class="subheading">Este blog foi montado por 2 amigos que amam Boku no Hero/My Hero Academya para
-          trazer novidades a respeito do mangá e anime e disponibilizar produtos personalizados da franquia.</span>
+        <div class="site-heading-4">
+          <h1 class="titulo-4">Sobre o Site</h1>
+          <span class="texto-4">Este blog foi montado por 2 amigos que amam Boku no Hero Academia, em tentar trazer
+            novidades a respeito do mangá e o anime, disponibilizando produtos personalizados da franquia.</span>
         </div>
       </div>
     </div>
   </div>
-</header>
+</div>
 
 @endsection
