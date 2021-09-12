3bfc3df8f097cebf9a9fa29b9142f37ccc2e9e0d
Josandonas <josandonas@outlook.com>
2021-09-11 23:50:05 -0400

    descartando fontes inuteis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,14 +37,7 @@
     <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <!-- Bootstrap core CSS
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    
      <!-- Custom fonts for this template -->
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
